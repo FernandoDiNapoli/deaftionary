cf272c1afd41b51b0e3e55d84b3f6384a52109a9
Fernando <fernandodinapoli1997@gmail.com>
2023-10-18 11:11:59 +0100

    Initial Commit

---
+++ Deaftionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Deaftionary
+//
+//  Created by user198631 on 18/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
