b21623d8e9d3e463a38340eece179eec5fb425a1
Profe Juan <juanrinconada@cev.com>
2024-02-20 11:32:42 +0000

    funciones agregadas a botones y restricciones a los campos de usuarios y contraseña

---
+++ Deaftionary/PantallaLoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  PantallaLoginViewController.swift
+//  Deaftionary
+//
+//  Created by user199132 on 20/02/2024.
+//
+
+import UIKit
+
+class PantallaLoginViewController: UIViewController {
+
+    @IBOutlet weak var usuarioTextField: UITextField!
+    @IBOutlet weak var contraseñaTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLoginButtonClick(_sender: UIButton){
+        let usuario = usuarioTextField.text
+        let contraseña = contraseñaTextField.text
+        
+        if usuario == "fernando" && contraseña == "12345"{
+            loginOk()
+        } else {
+            error()
+        }
+    }
+    
+    func loginOk(){
+        UserDefaults.standard.set(usuarioTextField.text,forKey: "usuario")
+        performSegue(withIdentifier: "toPantallaBienvenida", sender: nil)
+    }
+    
+    func error(){
+        usuarioTextField.textColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0, alpha: 1)
+        contraseñaTextField.textColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0, alpha: 1)
+}
+}
