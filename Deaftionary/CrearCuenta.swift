19862bbc5682d7f8e4f69bbe01a62e2e5ba88a5c
Profe Juan <juanrinconada@cev.com>
2024-02-26 12:53:59 +0000

    G

--- Deaftionary/CrearCuenta.swift
+++ Deaftionary/CrearCuenta.swift
@@ -8,30 +8,7 @@
 import UIKit
 
 class CrearCuenta: UIViewController {
-    @IBOutlet weak var nombreTextField: UITextField!
-    @IBOutlet weak var apellidoTextField: UITextField!
-    @IBOutlet weak var edadTextField: UITextField!
-    @IBOutlet weak var sexoTextField: UITextField!
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-    
-    @IBAction func onContinuarButtonClick(_sender: Any)
-    {
-        let nombre = nombreTextField.text
-        let apellido = apellidoTextField.text
-        let edad = edadTextField.text
-        let sexo = sexoTextField.text
-        let email = emailTextField.text
-        let contraseña = passwordTextField.text
-        
-   
-        }
-        
-    }
+    @IBAction func onCrearCieButtonClick(_sender: Any){
+        performSegue(withIdentifier: "PantallaABCS", sender: nil)    }
+}
     
